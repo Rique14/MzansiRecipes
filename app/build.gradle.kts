b64147e355cd823e158fff6ff4d4956f8b8b3bc3
Enrique Arendse <enriquearendse031@gmail.com>
2025-10-20 23:05:24 +0200

    Final POE.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
         applicationId = "com.mzansi.recipes"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
+        versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
